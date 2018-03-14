f1df6805022ccd4c4c81facd0d7bb3ff909c0d8d
Aleix Bueso <aleix.bueso@gmail.com>
2018-03-14 14:17:54 +0100

    Animation and Panel

---
+++ Assets/GenerateButtonPress.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateButtonPress : MonoBehaviour {
+
+    private void Awake()
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<Animator>().enabled = true;
+        GetComponent<Animator>().Play(0);
+        GetComponent<AudioSource>().Play();
+        StartCoroutine(ScreenDelay());
+    }
+
+    IEnumerator ScreenDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>().enabled = true;
+    }
+}
---
+++ Assets/SwitchPress.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPress : MonoBehaviour {
+
+    private void Awake()
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<Animator>().enabled = true;
+        GetComponent<Animator>().Play(0);
+        GetComponent<AudioSource>().Play();
+    }
+}
