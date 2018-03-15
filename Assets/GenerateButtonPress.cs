d518f8f49e50a20ea6223d9a11fbcae539d64bb5
Aleix Bueso <aleix.bueso@gmail.com>
2018-03-15 10:11:50 +0100

    Screen Working

--- Assets/GenerateButtonPress.cs
+++ Assets/GenerateButtonPress.cs
@@ -8,24 +8,22 @@ public class GenerateButtonPress : MonoBehaviour {
 
     private void Awake()
     {
-        GetComponent<Animator>().enabled = false;
-        screenImage.GetComponent<Animation>().Play("Logo");
+
     }
 
     private void OnMouseDown()
     {
-            GetComponent<Animator>().enabled = true;
-            GetComponent<Animator>().Play(0);
-            GetComponent<AudioSource>().Play();
-            screenImage.GetComponent<Animation>().Play("Generating");
-            StartCoroutine(ScreenDelay());
-
+        GetComponent<AudioSource>().Play();
+        screenImage.GetComponent<Animator>().SetBool("ButtonPressed", true);
+        StartCoroutine(ScreenDelay());
     }
 
     IEnumerator ScreenDelay()
     {
         yield return new WaitForSeconds(0.5f);
         GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>().enabled = true;
-        screenImage.GetComponent<Animation>().Play("Done!");
+
+        yield return new WaitForSeconds(2f);
+        screenImage.GetComponent<Animator>().SetBool("Generated", true);
     }
 }
